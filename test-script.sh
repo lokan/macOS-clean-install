5562475c85b697e2de25788985fd0d6179f4cbe3
lokan <lokansardari@gmail.com>
2020-04-05 07:56:06 +0200

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -10,3 +10,7 @@ chflags nohidden ~/Library
 # Pas de création de fichiers .DS_STORE
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+## MISSION CONTROL
+# Pas d'organisation des bureaux en fonction des apps ouvertes
+defaults write com.apple.dock mru-spaces -bool false
